e19e34018c6f9550e1009517b0248e17253eed79
IQBAL <Taha.Iqbal@NZ.MYTOWER.NET>
2020-01-09 16:54:38 +1300

    Swagger API docs added + lombok support added in project

--- src/main/java/nz/co/warehousegroup/springboot_sample/UserController.java
+++ src/main/java/nz/co/warehousegroup/springboot_sample/UserController.java
@@ -1,6 +1,5 @@
 package nz.co.warehousegroup.springboot_sample;
 
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,24 +7,29 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@Api(tags = "User API")
 @RestController
 public class UserController {
 
 	@Autowired
 	private UserService userService;
-	
-  @RequestMapping("/")
-  public String index() {	  
-    return "Greetings from Spring Boot!";
-  }
 
-  @RequestMapping("/users")
-  public List<Integer> getUsersByIds() {	  
-    return userService.getUserIds();
-  }
+	@ApiOperation(value = "Base path of user API")
+	@RequestMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
 
-  @RequestMapping("/user/{id}")
-  public User getUser(@PathVariable(name = "id", required=true ) Integer id) {	  
-    return userService.get(id);
-  }
+	@RequestMapping("/users")
+	public List<Integer> getUsersByIds() {
+		return userService.getUserIds();
+	}
+
+	@RequestMapping("/user/{id}")
+	public User getUser(@PathVariable(name = "id", required = true) Integer id) {
+		return userService.get(id);
+	}
 }
